dc50ff7076f2c8ed206e8b7b406bf740ecdde2ac
LuciferSixth <beamnaja75@gmail.com>
2024-06-23 12:12:52 +0700

    change content in Education page

--- src/app/_components/Education.tsx
+++ src/app/_components/Education.tsx
@@ -11,9 +11,10 @@ const Education = (props: Props) => {
           <div className="absolute -left-2 mt-1.5 h-3 w-3 rounded-full border border-white bg-blue-500"></div>
           <div className="rounded-lg bg-white p-2 shadow-md">
             <time className="text-sm font-semibold text-gray-500">
-              January 2024
+              January 2025
             </time>
             <h2 className="mb-2 text-sm font-semibold">Walailak University</h2>
+            <h2 className="mb-2 text-sm font-semibold">GPAX 3.98</h2>
           </div>
         </div>
         <div className="mb-5 ml-4">
@@ -22,7 +23,8 @@ const Education = (props: Props) => {
             <time className="text-sm font-semibold text-gray-500">
               March 2022
             </time>
-            <h2 className="mb-2 text-sm font-semibold">Panda School</h2>
+            <h2 className="mb-2 text-sm font-semibold">Saparachinee School</h2>
+            <h2 className="mb-2 text-sm font-semibold">GPAX 3.85</h2>
           </div>
         </div>
       </div>
