10bf36a170a13037bc11a4221bd40bf6a2ee60f1
LuciferSixth <beamnaja75@gmail.com>
2024-06-23 14:25:00 +0700

    change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        'blue' : 'rgb(59 130 246)'
+      }
     },
   },
   plugins: [],
